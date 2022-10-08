1599404428aacf5df74265753932d4988c5fc364
jspfei <jiangfeifan8@163.com>
2022-10-08 14:44:28 +0800

    提交 gin demo

---
+++ tool/Config.go
@@ -0,0 +1,32 @@
+package tool
+
+import (
+	"bufio"
+	"encoding/json"
+	"os"
+)
+
+type Config struct {
+	AppName string `json:"app_name"`
+	AppMode string `json:"app_mode"`
+	AppHost string `json:"app_host"`
+	AppPort string `json:"app_port"`
+}
+
+var _cfg *Config = nil
+
+func ParseConfig(path string) (*Config, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	decoer := json.NewDecoder(reader)
+	if err = decoer.Decode(&_cfg); err != nil {
+		return nil, err
+	}
+	return _cfg, nil
+}
